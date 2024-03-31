93659aa6406f63d61c61e16c6c328d2af9821237
Aviral Kaushik <zaviralkaushik@gmail.com>
2024-03-31 19:20:11 +0530

    Fixed the UI of ItemTile and implemented add to card dialog

--- lib/components/ItemTile.dart
+++ lib/components/ItemTile.dart
@@ -1,81 +1,109 @@
+import 'dart:ui';
+
 import 'package:flick/core/models/ShoppingItem.dart';
 import 'package:flick/utils/Colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ItemTile extends StatelessWidget {
   final ShoppingItem shoppingItem;
+  final void Function()? onTap;
 
-  const ItemTile({super.key, required this.shoppingItem});
+  const ItemTile({super.key, required this.shoppingItem, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: 25),
-      width: 200,
-      decoration: BoxDecoration(
-          color: shoppingCardBackground,
-          borderRadius: BorderRadius.circular(12)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // Shoe pic
-          ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Image.asset(shoppingItem.imagePath)),
-
-          // Description
-          Text(
-            shoppingItem.description,
-            style: TextStyle(color: quoteTextColor),
-          ),
-
-          // Price + Detail
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Item Name
-                    Text(
-                      shoppingItem.name,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    // Price
-                    Text(
-                      "\$ ${shoppingItem.price}",
-                      style: TextStyle(color: subTitleTextColor),
-                    )
-                  ],
-                ),
-
-                // Plus Button
-                Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                        color: blackColor,
-                        borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(12),
-                            bottomRight: Radius.circular(12))),
-                    child: Icon(
-                      Icons.add,
-                      color: whiteColor,
-                    ))
-              ],
+    return Stack(children: [
+      Container(
+        margin: const EdgeInsets.only(left: 25),
+        width: 200,
+        decoration: BoxDecoration(
+            color: shoppingCardBackground,
+            borderRadius: BorderRadius.circular(12)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // Shoe pic
+            Padding(
+              padding: const EdgeInsets.only(top: 12.0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    shoppingItem.imagePath,
+                    width: 150,
+                    height: 150,
+                  )),
             ),
-          )
 
-          // Button to add to cart
-        ],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      shoppingItem.description,
+                      style: TextStyle(color: quoteTextColor),
+                    ),
+                  ),
+
+                  // Price + Detail
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0, top: 25.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // Item Name
+                            Text(
+                              shoppingItem.name,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            // Price
+                            Text(
+                              "\$ ${shoppingItem.price}",
+                              style: TextStyle(color: subTitleTextColor),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-    );
+      Positioned(
+        bottom: 0,
+        right: 0,
+        child: GestureDetector(
+          onTap: onTap,
+          child: Container(
+              padding: const EdgeInsets.all(16),
+              margin: const EdgeInsets.only(left: 20.0),
+              decoration: BoxDecoration(
+                  color: blackColor,
+                  borderRadius: const BorderRadius.only(
+                      bottomRight: Radius.circular(12),
+                      topLeft: Radius.circular(12))),
+              child: Icon(
+                Icons.add,
+                color: whiteColor,
+              )),
+        ),
+      )
+    ]);
   }
 }
--- lib/features/shop/screen/ShopScreen.dart
+++ lib/features/shop/screen/ShopScreen.dart
@@ -1,7 +1,9 @@
 import 'package:flick/components/ItemTile.dart';
+import 'package:flick/core/models/CartItem.dart';
 import 'package:flick/core/models/ShoppingItem.dart';
 import 'package:flick/utils/Colors.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ShopScreen extends StatefulWidget {
   const ShopScreen({super.key});
@@ -11,85 +13,100 @@ class ShopScreen extends StatefulWidget {
 }
 
 class _ShopScreenState extends State<ShopScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // search bar
-        _searchBar(),
+  void addShoppingItemToCart(ShoppingItem shoppingItem) {
+    Provider.of<CartItem>(context, listen: false).addItemToCart(shoppingItem);
 
-        // message
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 25.0),
-          child: Text(
-            "everyone flies... some fly longer than others",
-            style: TextStyle(color: quoteTextColor),
-          ),
-        ),
-
-        // hot picks
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              const Text(
-                "Hot Picks 🔥",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-              ),
-              Text(
-                "See all",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold, color: linkTextColor),
-              )
-            ],
-          ),
-        ),
-
-        const SizedBox(
-          height: 10,
-        ),
-
-        Expanded(
-            child: ListView.builder(
-                itemCount: 5,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  ShoppingItem shoppingItem = ShoppingItem(
-                      name: "Air Jordan",
-                      price: "240",
-                      imagePath: "lib/images/air_jordan.png",
-                      description: "Cool Shoe");
-                  return ItemTile(
-                    shoppingItem: shoppingItem,
-                  );
-                })),
-
-        Padding(
-          padding: const EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),
-          child: Divider(color: whiteTextColor,),
-        )
-      ],
-    );
+    showDialog(
+        context: context,
+        builder: (context) => const AlertDialog(
+              title: Text("Successfully Added!"),
+              content: Text("Check Your Cart!"),
+            ));
   }
 
-  _searchBar() {
-    Container(
-        padding: const EdgeInsets.all(12),
-        margin: const EdgeInsets.symmetric(horizontal: 25),
-        decoration: BoxDecoration(
-            color: searchBarBackgroundColor,
-            borderRadius: BorderRadius.circular(8)),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Icon(Icons.search),
-            Text(
-              "Search",
-              style: TextStyle(color: subTitleTextColor),
-            ),
-          ],
-        ));
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CartItem>(
+        builder: (context, value, child) => Column(
+              children: [
+                // search bar
+                Container(
+                    padding: const EdgeInsets.all(12),
+                    margin: const EdgeInsets.symmetric(horizontal: 25),
+                    decoration: BoxDecoration(
+                        color: searchBarBackgroundColor,
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Icon(Icons.search),
+                        Text(
+                          "Search",
+                          style: TextStyle(color: subTitleTextColor),
+                        ),
+                      ],
+                    )),
+
+                // message
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 25.0),
+                  child: Text(
+                    "everyone flies... some fly longer than others",
+                    style: TextStyle(color: quoteTextColor),
+                  ),
+                ),
+
+                // hot picks
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        "Hot Picks 🔥",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 24),
+                      ),
+                      Text(
+                        "See all",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: linkTextColor),
+                      )
+                    ],
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+
+                // list of shoes for sale
+                Expanded(
+                    child: ListView.builder(
+                        itemCount: value.getShoppingItemOnSale().length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          ShoppingItem shoppingItem =
+                              value.getShoppingItemOnSale()[index];
+
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 30.0),
+                            child: ItemTile(
+                              shoppingItem: shoppingItem,
+                              onTap: () => addShoppingItemToCart(shoppingItem),
+                            ),
+                          );
+                        })),
+
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),
+                  child: Divider(
+                    color: whiteTextColor,
+                  ),
+                )
+              ],
+            ));
   }
 }
