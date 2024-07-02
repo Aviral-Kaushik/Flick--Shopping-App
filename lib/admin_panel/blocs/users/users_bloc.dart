f1f1e233fcad427326a70814685e134c71093444
Aviral Kaushik <zaviralkaushik@gmail.com>
2024-07-02 11:36:08 +0530

    Implemented User Bloc

--- lib/admin_panel/blocs/users/users_bloc.dart
+++ lib/admin_panel/blocs/users/users_bloc.dart
@@ -1,20 +1,72 @@
 import 'package:flick/admin_panel/blocs/users/users_event.dart';
 import 'package:flick/admin_panel/blocs/users/users_state.dart';
 import 'package:flick/admin_panel/repositories/users_repository.dart';
+import 'package:flick/models/User.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tuple/tuple.dart';
 
 class UsersBloc extends Bloc<UsersEvent, UsersState> {
   final UsersRepository usersRepository;
 
   UsersBloc(this.usersRepository) : super(UsersStateInitial()) {
-    on<FetchAllUsers>((event, emit) async {});
+    on<FetchAllUsers>((event, emit) async {
+      emit(const UsersLoading("Please wait! Fetching users data"));
 
-    on<SearchUser>((event, emit) async {});
+      Tuple3<List<User>, bool, String> usersResponse =
+          await usersRepository.fetchAllUsers();
 
-    on<EditUser>((event, emit) async {});
+      if (usersResponse.item2) {
+        emit(UsersError("Cannot Load User: ${usersResponse.item3}"));
+      } else {
+        emit(FetchedAllUsers(usersResponse.item1));
+      }
 
-    on<ApplyFilter>((event, emit) async {});
+    });
 
-    on<DeleteUser>((event, emit) async {});
+    on<SearchUser>((event, emit) async {
+      emit(const UsersLoading("Searching ..."));
+
+      List<User> users = await usersRepository.searchUsers(event.searchQuery);
+
+      emit(FetchedAllUsers(users));
+    });
+
+    on<EditUser>((event, emit) async {
+      emit(const UsersLoading("Updating User! Please wait"));
+
+      Tuple2<bool, String> userUpdateResponse =
+          await usersRepository.editUser(event.userToBeEdited);
+
+      if (userUpdateResponse.item1) {
+        emit(const UserEditedSuccessfully());
+      } else {
+        emit(const UsersError("Cannot Update User at this moment"));
+      }
+
+    });
+
+    on<ApplyFilter>((event, emit) async {
+      emit(const UsersLoading("Applying filter! Please Wait"));
+
+      List<User> filteredUsers =
+          await usersRepository.applyUserFilter(event.userFilter);
+
+      emit(FetchedAllUsers(filteredUsers));
+    });
+
+    on<DeleteUser>((event, emit) async {
+      emit(const UsersLoading("Please Wait! Deleting User"));
+
+      Tuple2<bool, String> deletedResponse =
+          await usersRepository.deleteUser(event.user);
+
+      if (deletedResponse.item1) {
+        emit(const UserDeletedSuccessfully());
+      } else {
+        emit(UsersError(
+            "Cannot Delete User at this moment ${deletedResponse.item2}"));
+      }
+
+    });
   }
 }
--- lib/admin_panel/repositories/users_repository.dart
+++ lib/admin_panel/repositories/users_repository.dart
@@ -1,27 +1,38 @@
 import 'package:flick/admin_panel/helper/UserFilter.dart';
 import 'package:flick/models/User.dart';
+import 'package:tuple/tuple.dart';
 
 class UsersRepository {
 
   late List<User> users;
 
-  Future<List<User>> fetchAllUsers() async {
-    return List.empty();
+  Future<Tuple3<List<User>, bool, String>> fetchAllUsers() async {
+    List<User> users = List.empty();
+    bool hasErrorOccurred = false;
+    String errorMessage = "";
+
+    return Tuple3(users, hasErrorOccurred, errorMessage);
   }
 
   Future<List<User>> searchUsers(String query) async {
     return List.empty();
   }
 
-  Future<bool> editUser(User user) async {
-    return true;
+  Future<Tuple2<bool, String>> editUser(User user) async {
+    bool isUserUpdatedSuccessfully = true;
+    String errorMessage = "";
+
+    return Tuple2(isUserUpdatedSuccessfully, errorMessage);
   }
 
   Future<List<User>> applyUserFilter(UserFilter userFilter) async {
     return List.empty();
   }
 
-  Future<bool> deleteUser(User user) async {
-    return true;
+  Future<Tuple2<bool, String>> deleteUser(User user) async {
+    bool isUserDeletedSuccessfully = false;
+    String errorMessage = "Firebase Exception";
+
+    return Tuple2(isUserDeletedSuccessfully, errorMessage);
   }
 }
